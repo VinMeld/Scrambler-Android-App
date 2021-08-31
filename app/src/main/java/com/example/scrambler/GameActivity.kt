e082ce23a79b822b4cde902a00f050fd4e10804d
Vinaymeldrum <36042425+Vinaymeldrum@users.noreply.github.com>
2021-08-30 23:55:50 -0400

    Cleaned up timer (#6)
    Co-authored-by: vinaycat <vinaymeldrum@gmail.com>

--- app/src/main/java/com/example/scrambler/GameActivity.kt
+++ app/src/main/java/com/example/scrambler/GameActivity.kt
@@ -6,13 +6,13 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import android.os.Bundle
 import com.example.scrambler.R
-import android.widget.TextView
-import android.widget.EditText
-import android.widget.Toast
 import android.content.Intent
+import android.text.method.KeyListener
 import android.util.Log
 import android.view.View
-import android.widget.Button
+import android.widget.*
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
 import com.android.volley.Request
 import com.example.scrambler.MenuActivity
 import com.android.volley.RequestQueue
@@ -21,77 +21,92 @@ import com.android.volley.toolbox.StringRequest
 import com.example.scrambler.GameActivity
 import com.android.volley.VolleyError
 import com.google.firebase.firestore.FieldValue
+import kotlinx.coroutines.*
+import java.lang.Thread.sleep
 import java.util.*
 
-class GameActivity : AppCompatActivity() {
-    var word = ""
-    var correct = 0
-    var chances = 3
-    var seconds = 0
-    var firebaseUser = FirebaseAuth.getInstance().currentUser
-    var userID = firebaseUser!!.uid
-    var db = FirebaseFirestore.getInstance()
-    var user = db.collection("Users").document(userID)
+class GameActivity : AppCompatActivity(), View.OnClickListener {
+    private var word = ""
+    private var correct = 0
+    private var chances = 3
+    private var seconds = 0
+    private var firebaseUser = FirebaseAuth.getInstance().currentUser
+    private var userID = firebaseUser!!.uid
+    private var db = FirebaseFirestore.getInstance()
+    private var user = db.collection("Users").document(userID)
+    private var correctGuess = false
+    private val scopeTimer = CoroutineScope(CoroutineName("Timer"))
+    private var menu: Button? = null
+    private var correctWords: TextView? = null
+    private var scrambledWord: TextView? = null
+    private var enterScramble: EditText? = null
+    private var textViewChances: TextView? = null
+    private var buttonRestart: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        val menu = findViewById<Button>(R.id.buttonMenu)
-        val correctWords = findViewById<TextView>(R.id.textViewCorrect)
-        val scrambledWord = findViewById<TextView>(R.id.textWord)
-        val enterScramble = findViewById<EditText>(R.id.textViewWord)
-        val textViewChances = findViewById<TextView>(R.id.textViewChances)
-        val buttonRestart = findViewById<Button>(R.id.buttonRestart)
-        correctWords.text = correct.toString()
-        textViewChances.text = chances.toString()
+        menu = findViewById(R.id.buttonMenu)
+        menu?.setOnClickListener(this)
+        correctWords = findViewById(R.id.textViewCorrect)
+        scrambledWord = findViewById(R.id.textWord)
+        enterScramble = findViewById(R.id.textViewWord)
+        textViewChances = findViewById(R.id.textViewChances)
+        buttonRestart = findViewById(R.id.buttonRestart)
+        correctWords!!.text = correct.toString()
+        textViewChances!!.text = chances.toString()
         val timerText = findViewById<TextView>(R.id.textViewTimer)
-        word = setScrambledWord(scrambledWord)
-        startGame(scrambledWord, enterScramble, textViewChances, timerText)
+        word = setScrambledWord(scrambledWord!!)
+        startGame(scrambledWord!!, enterScramble!!, textViewChances!!, timerText!!)
         // RESTART BUTTON
-        buttonRestart.setOnClickListener {
+        buttonRestart!!.setOnClickListener {
             if (chances > 0) {
                 val toast = Toast.makeText(applicationContext, "Keep trying! You're not done yet", Toast.LENGTH_SHORT)
                 toast.show()
             } else {
                 chances = 3
                 correct = 0
-                correctWords.text = correct.toString()
-                textViewChances.text = chances.toString()
-                enterScramble.visibility = View.VISIBLE
-                scrambledWord.visibility = View.VISIBLE
-                word = setScrambledWord(scrambledWord)
-                startGame(scrambledWord, enterScramble, textViewChances, timerText)
+                correctWords!!.text = correct.toString()
+                textViewChances!!.text = chances.toString()
+                enterScramble!!.visibility = View.VISIBLE
+                scrambledWord!!.visibility = View.VISIBLE
+                sleep(1000)
+                word = setScrambledWord(scrambledWord!!)
+                startGame(scrambledWord!!, enterScramble!!, textViewChances!!, timerText)
             }
         }
-        // MENU
-        menu.setOnClickListener { startActivity(Intent(this@GameActivity, MenuActivity::class.java)) }
         // CHECK IF TYPED CORRECTLY
-        enterScramble.setOnKeyListener { _, _, _ -> // IF ANSWER IS CORRECT
-            if (enterScramble.text.toString().contentEquals(word)) {
-                seconds = 1
-                correct++
-                correctWords.text = correct.toString()
-                enterScramble.setText("")
-                word = setScrambledWord(scrambledWord)
-            }
+        enterScramble!!.setOnKeyListener { _, _, _ -> // IF ANSWER IS CORRECT
+            checkIfCorrect()
             false
         }
     }
-
+    private fun checkIfCorrect(){
+        if (enterScramble!!.text.toString().contentEquals(word)) {
+            enterScramble!!.setText("")
+            correct++
+            correctWords!!.text = correct.toString()
+            sleep(1000L)
+            correctGuess = true
+            word = setScrambledWord(scrambledWord!!)
+        }
+    }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.buttonMenu -> startActivity(Intent(this, MenuActivity::class.java))
+        }
+    }
     protected fun randomNumber(low: Int, high: Int): Int {
         val r = Random()
         return r.nextInt(high - low) + low
     }
 
     protected fun setScrambledWord(scrambledWord: TextView): String {
+        Log.e(TAG, "Scrambling word")
         val wordNumber = randomNumber(1, 1000)
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://most-common-words.herokuapp.com/api/search?top=" +
                 wordNumber
-        //        if(word.isEmpty()){
-//            word = "a";
-//        }
-        // while (word.length() < 3) {
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(Request.Method.GET, url,
                 { response ->
@@ -114,44 +129,64 @@ class GameActivity : AppCompatActivity() {
                             }
                         }
                         val randomWordScrambled = String(arrayOfCharacters)
-                        // Display the first 500 characters of the response string.
                         scrambledWord.text = randomWordScrambled
+                        seconds = 0
                     }
                 }) { scrambledWord.text = "That didn't work!" }
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
-        // }
         return word
     }
 
     protected fun startGame(scrambledWord: TextView, enterScramble: EditText, textViewChances: TextView, timerText: TextView) {
         Log.e(TAG, "StartGame")
-        val timer = Timer()
-        timer.schedule(object : TimerTask() {
-            override fun run() {
-                if (seconds < 10) {
+        sleep(100L)
+        scopeTimer.launch(Dispatchers.Default) {
+            val job1 = launch {
+                while(seconds < 11) {
                     runOnUiThread { timerText.text = seconds.toString() }
                     Log.e(TAG, "Seconds = $seconds")
+                    delay(1000L)
                     seconds++
-                } else {
-                    if (chances == 1) {
-                        runOnUiThread {
-                            enterScramble.visibility = View.INVISIBLE
-                            scrambledWord.visibility = View.INVISIBLE
-                        }
-                        timer.cancel()
-                        // ADDING SCORE TO DATABASE
-                        user.update("scores", FieldValue.arrayUnion(correct))
-                    }
-                    word = setScrambledWord(scrambledWord)
-                    chances--
-                    runOnUiThread { textViewChances.text = chances.toString() }
-                    seconds = 1
                 }
             }
-        }, 0, 1000)
+            delay(10000L)
+            while(!job1.isCompleted){
+                delay(100)
+            }
+            Log.e(TAG, seconds.toString())
+            if (chances == 1) {
+                chances--
+                runOnUiThread {
+                    textViewChances.text = chances.toString()
+                    enterScramble.visibility = View.INVISIBLE
+                    scrambledWord.visibility = View.INVISIBLE
+                }
+                user.update("scores", FieldValue.arrayUnion(correct))
+                runOnUiThread { textViewChances.text = chances.toString() }
+            } else if(job1.isCompleted){
+                Log.e(TAG, "here")
+                job1.cancelAndJoin()
+                chances--
+                runOnUiThread { textViewChances.text = chances.toString() }
+                delay(1000L)
+                seconds = 1
+                word = setScrambledWord(scrambledWord)
+                startGame(scrambledWord, enterScramble, textViewChances, timerText)
+            } else if (correctGuess) {
+                job1.cancelAndJoin();
+                correctGuess = false
+            }
+        }
     }
 
+    override fun onStop() {
+        user.update("scores", FieldValue.arrayUnion(correct))
+        runBlocking {
+            scopeTimer.cancel()
+        }
+        super.onStop()
+    }
     companion object {
         private const val TAG = "GameActivity"
     }
